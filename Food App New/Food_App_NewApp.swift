311db275391e44b3b329a2865958601cc8e1d315
Danciu David <98606974+XyzDavidD@users.noreply.github.com>
2025-10-16 13:27:56 +0300

    Initial Commit

---
+++ Food App New/Food_App_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_App_NewApp.swift
+//  Food App New
+//
+//  Created by Danciu David on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Food_App_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Food App NewTests/Food_App_NewTests.swift
@@ -0,0 +1,17 @@
+//
+//  Food_App_NewTests.swift
+//  Food App NewTests
+//
+//  Created by Danciu David on 16.10.2025.
+//
+
+import Testing
+@testable import Food_App_New
+
+struct Food_App_NewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
